0587637d29db29279585b9dc86d338dd9c7bef73
Witalon Rodrigues <162512211+witalonrodrigues@users.noreply.github.com>
2025-04-27 17:39:21 -0300

    feat - adicionando arquivo para colocar credencias aws

---
+++ certificates.h
@@ -0,0 +1,17 @@
+const char* aws_cert_ca = R"EOF(
+-----BEGIN CERTIFICATE-----
+// INSERIR SUA CHAVE AWS
+-----END CERTIFICATE-----
+)EOF";
+
+const char* aws_cert_crt = R"KEY(
+-----BEGIN CERTIFICATE-----
+// INSERIR SUA CHAVE AWS
+-----END CERTIFICATE-----
+)KEY";
+
+const char* aws_cert_private = R"KEY(
+-----BEGIN RSA PRIVATE KEY-----
+// INSERIR SUA CHAVE AWS
+-----END RSA PRIVATE KEY-----
+)KEY";
